466d20c75ffd054532330653c98dd10357306c58
ibutaibeh <ibutaibeh@hotmail.com>
2025-07-16 21:49:07 +0300

    update homework

---
+++ app.js
@@ -0,0 +1,40 @@
+// ----------------------------------------------------------------
+//Exercise 2 
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+const [firstIngredient,secondIngredient]=pizzaToppings;
+console.log(`The First ingredient = ${firstIngredient} - The Second ingredient ${secondIngredient}`);
+// ----------------------------------------------------------------
+//Exercise 3
+const car = {
+  make: 'Audi',
+  model: 'Q5',
+};
+const {make, model}= car;
+console.log(`Car make is ${make} and Car model is ${model}`)
+// ----------------------------------------------------------------
+//Exercise 4
+const morePizzaToppings = ['Cheese', 'Sauce'];
+const uncontroversialPizzaToppings= [...morePizzaToppings]
+console.log(`Pizza Topping ${uncontroversialPizzaToppings}`);
+// ----------------------------------------------------------------
+//Exercise 5
+
+// ----------------------------------------------------------------
+//Exercise 6
+
+// ----------------------------------------------------------------
+//Exercise 7
+
+// ----------------------------------------------------------------
+//Exercise 8
+
+// ----------------------------------------------------------------
+//Exercise 9
+
+// ----------------------------------------------------------------
+//Exercise 10
+
+// ----------------------------------------------------------------
+//Exercise 11
+
