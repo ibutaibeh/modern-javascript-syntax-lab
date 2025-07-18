6db0b60d9b87b17c6c834eb7ff9173f3fa1a7c48
ibutaibeh <ibutaibeh@hotmail.com>
2025-07-18 13:11:04 +0300

    finish HW

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // ----------------------------------------------------------------
-//Exercise 2 
+//Exercise 1- The map() iterator method of arrays
+ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+ const numsTimesTwo= nums.map((num)=>(num*2))
+
+ console.log('The New Array = ', numsTimesTwo)
+// ----------------------------------------------------------------
+//Exercise 2 Array destructuring
 const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 const [firstIngredient,secondIngredient]=pizzaToppings;
 console.log(`The First ingredient = ${firstIngredient} - The Second ingredient ${secondIngredient}`);
 // ----------------------------------------------------------------
-//Exercise 3
+//Exercise 3 Object destructuring
 const car = {
   make: 'Audi',
   model: 'Q5',
@@ -13,12 +20,12 @@ const car = {
 const {make, model}= car;
 console.log(`Car make is ${make} and Car model is ${model}`)
 // ----------------------------------------------------------------
-//Exercise 4
+//Exercise 4 Applying the spread operator on arrays
 const morePizzaToppings = ['Cheese', 'Sauce'];
 const uncontroversialPizzaToppings= [...morePizzaToppings]
 console.log(`Pizza Topping ${uncontroversialPizzaToppings}`);
 // ----------------------------------------------------------------
-//Exercise 5
+//Exercise 5 Applying the spread operator on objects
 const anotherCar = {
   make: 'Toyota',
   model: 'RAV4',
@@ -29,7 +36,7 @@ myCar.make= 'Tank';
 myCar.model= '300';
 console.log('First Car = ',anotherCar,'Second Car =', myCar);
 // ----------------------------------------------------------------
-//Exercise 6
+//Exercise 6 Dynamic keys in objects
 const propertyName = 'age';
 const userProfile = {
   [propertyName]: 5
@@ -37,17 +44,35 @@ const userProfile = {
 
 console.log('User Profile info : ', userProfile);
 // ----------------------------------------------------------------
-//Exercise 7
+//Exercise 7 Import and Export
+// completed
+// ----------------------------------------------------------------
+//Exercise 8 Default parameters
+function writeIt(noun='cat',adjectives='orange'){
+  return console.log(`The ${noun} is ${adjectives}!`)
+}
+writeIt();
+writeIt('dog','black');
+// ----------------------------------------------------------------
+//Exercise 9 Ternary operator
+const pizza = 'tasty';
+pizza==='tasty' ? console.log('Yum!') : console.log('Yuck!');
 
 // ----------------------------------------------------------------
-//Exercise 8
+//Exercise 10 Boolean gates
+const localLangConfig = null
+const lang= localLangConfig || 'en';
+console.log(lang);
 
+const savedUserTheme = null
+const theme= savedUserTheme || 'Light'
+console.log(theme)
 // ----------------------------------------------------------------
-//Exercise 9
+//Exercise 11 Optional chaining '?'
+const adventurer = {
+  name: 'Alice',
+};
 
-// ----------------------------------------------------------------
-//Exercise 10
+console.log(adventurer.cat?.age);
 
-// ----------------------------------------------------------------
-//Exercise 11
 
